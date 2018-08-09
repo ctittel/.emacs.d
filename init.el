8428b414988caef6f9ac71232be9bb57afd77ed4
Christoph <christoph.tittel@gmail.com>
2018-08-09 21:01:30 +0200

    initial

---
+++ init.el
@@ -0,0 +1,90 @@
+; == Meine emacs config
+
+(require 'package)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+
+(package-initialize)
+
+
+					;=========== Variablen und Einstellungen für Packages
+					;==================================================
+
+					;---------- Lade nicht vorhandene packages
+(unless package-archive-contents
+  (package-refresh-contents))
+(package-install-selected-packages)
+
+
+
+					;------------- Backup Dateien
+(setq
+ backup-by-copying t      ; don't clobber symlinks
+ delete-old-versions t
+ kept-new-versions 6
+ kept-old-versions 2
+ version-control t       ; use versioned backups
+ )
+(setq auto-save-file-name-transforms `((".*" . ,temporary-file-directory)))
+(setq backup-directory-alist `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
+
+					;----------- Startup Screen
+(setq inhibit-startup-screen t)
+					;-------------------- Menu Bar und Tool Bar
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+
+					;------------ bell
+(setq visible-bell t)
+
+					;--------------- helm
+(require 'helm)
+(require 'helm-config)
+
+;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
+;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
+;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-unset-key (kbd "C-x c"))
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-c h o") 'helm-occur)
+(global-set-key (kbd "C-x r b") 'helm-bookmarks)
+
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB work in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
+(when (executable-find "curl")
+  (setq helm-google-suggest-use-curl-p t))
+
+(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+      helm-ff-file-name-history-use-recentf t
+      helm-echo-input-in-header-line t)
+
+					;(defun spacemacs//helm-hide-minibuffer-maybe ()
+					; "Hide minibuffer in Helm session if we use the header line as input field."
+					;(when (with-helm-buffer helm-echo-input-in-header-line)
+					; (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
+					;  (overlay-put ov 'window (selected-window))
+					; (overlay-put ov 'face
+					;             (let ((bg-color (face-background 'default nil)))
+					;              `(:background ,bg-color :foreground ,bg-color)))
+					;(setq-local cursor-type nil))))
+
+
+					;(add-hook 'helm-minibuffer-set-up-hook
+					;         'spacemacs//helm-hide-minibuffer-maybe)
+
+(setq helm-autoresize-max-height 0)
+(setq helm-autoresize-min-height 20)
+(helm-autoresize-mode 1)
+
+(helm-mode 1)
